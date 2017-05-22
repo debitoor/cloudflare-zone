781ca976f3dd58f87b6ccded972bd2005fc24633
Jonatan Pedersen <jp@jonatanpedersen.com>
2017-05-22 09:49:54 +0200

    builds src

--- bin/cloudflare-zone.js
+++ bin/cloudflare-zone.js
@@ -9,12 +9,12 @@ var _cloudflareZone = require('../lib/cloudflare-zone');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const optionDefinitions = [{ name: 'file', type: String }, { name: 'authEmail', type: String, defaultValue: process.env.CLOUDFLARE_AUTH_EMAIL }, { name: 'authKey', type: String, defaultValue: process.env.CLOUDFLARE_AUTH_KEY }];
+const optionDefinitions = [{ name: 'file', type: String }, { name: 'authEmail', type: String, defaultValue: process.env.CLOUDFLARE_AUTH_EMAIL }, { name: 'authKey', type: String, defaultValue: process.env.CLOUDFLARE_AUTH_KEY }, { name: 'autoCreate', type: Boolean, defaultValue: false }];
 
 const options = (0, _commandLineArgs2.default)(optionDefinitions);
 
 (0, _cloudflareZone.main)(options).catch(err => {
-	console.error(err);
+	console.error(err, err.stack);
 	process.exit(1);
 }).then(() => {
 	process.exit(0);
